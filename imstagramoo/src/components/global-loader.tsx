f6f828bf8c96ac1c692cc90edee5d2af85ae2b0a
imnamgi <happynj2697@gmail.com>
2025-11-01 19:28:45 +0900

    Design: loading page #06

--- imstagramoo/src/components/global-loader.tsx
+++ imstagramoo/src/components/global-loader.tsx
@@ -1,3 +1,11 @@
+import logo from "@/assets/imstalogo.png";
 export default function GlobalLoader() {
-  return <div>로딩딩</div>;
+  return (
+    <div className="bg-muted flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mb-15 flex animate-bounce items-center gap-4">
+        <img src={logo} className="w-10" alt="imstagramoo logo" />
+        <div className="text-2xl font-bold">imstagramoo</div>
+      </div>
+    </div>
+  );
 }
