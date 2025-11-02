4a29e6eac5f60390d90391ae1d172e2a2f79734a
imnamgi <happynj2697@gmail.com>
2025-11-02 17:59:34 +0900

    Feat: userProfile 조회 #06

--- imstagramoo/src/provider/session-provider.tsx
+++ imstagramoo/src/provider/session-provider.tsx
@@ -1,17 +1,23 @@
 import type { ReactNode } from "react";
 import supabase from "@/lib/supabase";
 import { useEffect } from "react";
-import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useIsSessionLoaded, useSession, useSetSession } from "@/store/session";
 import GlobalLoader from "@/components/global-loader";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
 
 export default function SessionProvider({ children }: { children: ReactNode }) {
+  const session = useSession();
   const setSession = useSetSession();
   const isSessionLoaded = useIsSessionLoaded();
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
       setSession(session);
     });
   }, []);
   if (!isSessionLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />;
   return children;
 }
