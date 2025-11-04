efce97d9388c2d7967fae2c1a85e2a0b27ec39c0
imnamgi <happynj2697@gmail.com>
2025-11-04 11:25:32 +0900

    Feat: 이미지 storage 연동 #07

--- imstagramoo/src/api/post.ts
+++ imstagramoo/src/api/post.ts
@@ -1,9 +1,77 @@
+import { uploadImage } from "@/api/image";
 import supabase from "@/lib/supabase";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({
-    content,
-  });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({
+      content,
+    })
+    .select()
+    .single(); // insert는 원래 return data가 없으니까 select해서 data가져오기
   if (error) throw error;
   return data;
+}
+
+// Partial타입으로 값하나만 업데이트 하도록
+// 단, 인터섹션타입으로 id를 추가하여, id는 꼭 추가하도록
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {
+  const { data, error } = await supabase
+    .from("post")
+    .update(post)
+    .eq("id", post.id)
+    .select()
+    .single();
+  if (error) throw error;
+  return data;
+}
+
+export async function deletePost(id: number) {
+  const { data, error } = await supabase
+    .from("post")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
+  if (error) throw error;
+  return data;
+}
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  // post insert
+  const post = await createPost(content);
+  if (images.length === 0) return post;
+  try {
+    // image upload 병렬처리
+    // return되는 업로드 비동기함수 배열이 Promise.all에 의해 병렬처리
+    const imageUrls = await Promise.all(
+      images.map((image) => {
+        const fileExtension = image.name.split(".").pop() || "webp";
+        const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExtension}`;
+        const filePath = `${userId}/${post.id}/${fileName}`;
+        return uploadImage({
+          file: image,
+          filePath,
+        });
+      }),
+    );
+    // post table update
+    const updatedPost = await updatePost({
+      id: post.id,
+      "image-urls": imageUrls,
+    });
+    return updatedPost;
+  } catch (error) {
+    await deletePost(post.id);
+    throw error;
+  }
 }
