3b29eb7298c3f76efc15f22a3960ab891a2eb7a7
imnamgi <happynj2697@gmail.com>
2025-11-07 14:49:21 +0900

    Feat: post caching normalization #07

--- imstagramoo/src/components/post/post-item.tsx
+++ imstagramoo/src/components/post/post-item.tsx
@@ -11,10 +11,23 @@ import { formatTimeAgo } from "@/lib/time";
 import EditPostButton from "@/components/post/edit-post-button";
 import DeletePostButton from "@/components/post/delete-post-button";
 import { useSession } from "@/store/session";
+import { usePostbyIdData } from "@/hooks/queries/use-post-by-id-data";
+import Loader from "@/components/loader";
+import Fallback from "@/components/fallback";
 
-export default function PostItem(post: Post) {
+export default function PostItem({ postId }: { postId: number }) {
   const session = useSession();
   const userId = session?.user.id;
+  const {
+    data: post,
+    error,
+    isPending,
+  } = usePostbyIdData({
+    postId,
+    type: "FEED",
+  });
+  if (isPending) return <Loader />;
+  if (error) return <Fallback />;
   const isMine = post.author_id === userId;
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
