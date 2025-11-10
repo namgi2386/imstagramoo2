19a83b278c3315f60ba1f147c3e73a80459b1c09
imnamgi <happynj2697@gmail.com>
2025-11-10 15:12:45 +0900

    Feat: update comment #10

--- imstagramoo/src/pages/post-detail-page.tsx
+++ imstagramoo/src/pages/post-detail-page.tsx
@@ -11,8 +11,8 @@ export default function PostDetailPage() {
     <div className="flex flex-col gap-5">
       <PostItem postId={Number(postId)} type="DETAIL" />
       <div className="text-xl font-bold">댓글</div>
-      <CommentEditor postId={Number(postId)} />
-      <CommentList postId={Number(postId)}/>
+      <CommentEditor type="CREAT" postId={Number(postId)} />
+      <CommentList postId={Number(postId)} />
     </div>
   );
 }
