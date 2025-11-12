49752ea4989dc58135dacf67a3f61454df6608fa
imnamgi <happynj2697@gmail.com>
2025-11-12 12:46:33 +0900

    Feat: comment - root_comment_id를 통한 대대댓글 처리 #10

--- imstagramoo/src/components/comment/comment-item.tsx
+++ imstagramoo/src/components/comment/comment-item.tsx
@@ -39,6 +39,7 @@ export default function CommentItem(props: NestedComment) {
   };
   const isMine = session?.user.id === props.author.id;
   const isRootComment = props.parentComment === undefined;
+  const isOverTwoLevels = props.parent_comment_id !== props.root_comment_id;
   return (
     <div
       className={`flex flex-col gap-8 ${isRootComment ? "border-b" : "ml-6"} pb-5`}
@@ -62,7 +63,14 @@ export default function CommentItem(props: NestedComment) {
               onClose={toggleIsEditing}
             />
           ) : (
-            <div>{props.content}</div>
+            <div>
+              {isOverTwoLevels && (
+                <span className="font-bold text-blue-500">
+                  @{props.parentComment?.author.nickname}
+                </span>
+              )}
+              {props.content}
+            </div>
           )}
           <div className="text-muted-foreground flex justify-between text-sm">
             <div className="flex items-center gap-2">
@@ -100,6 +108,7 @@ export default function CommentItem(props: NestedComment) {
               type="REPLY"
               postId={props.post_id}
               parentCommentId={props.id}
+              rootCommentId={props.root_comment_id || props.id}
               onClose={toggleIsReply}
             />
           )}
--- imstagramoo/src/components/comment/comment-list.tsx
+++ imstagramoo/src/components/comment/comment-list.tsx
@@ -7,16 +7,21 @@ import type { Comment, NestedComment } from "@/types";
 function toNestedComments(comments: Comment[]): NestedComment[] {
   const result: NestedComment[] = [];
   comments.forEach((comment) => {
-    if (!comment.parent_comment_id) {
+    if (!comment.root_comment_id) {
       result.push({ ...comment, children: [] });
     } else {
-      const parentCommentIndex = result.findIndex(
+      const rootCommentIndex = result.findIndex(
+        (item) => item.id === comment.root_comment_id,
+      );
+      const parentComment = comments.find(
         (item) => item.id === comment.parent_comment_id,
       );
-      result[parentCommentIndex].children.push({
+      if (rootCommentIndex === -1) return;
+      if (!parentComment) return;
+      result[rootCommentIndex].children.push({
         ...comment,
         children: [],
-        parentComment: result[parentCommentIndex],
+        parentComment,
       });
     }
   });
@@ -31,7 +36,7 @@ export default function CommentList({ postId }: { postId: number }) {
   } = useCommentsData(postId);
   if (fetchCommentsError) return <Fallback />;
   if (isFetchCommentsPending) return <Loader />;
-  const nestedComments = toNestedComments(comments)
+  const nestedComments = toNestedComments(comments);
   return (
     <div className="flex flex-col gap-5">
       {nestedComments.map((comment) => (
